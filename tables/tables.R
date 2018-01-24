0ce1b24990ddfe0f2eeef86e1637d80416b84d53
johnwoodill <johnwoodill@gmail.com>
2018-01-23 19:15:21 -1000

    adding regression

--- tables/tables.R
+++ tables/tables.R
@@ -47,7 +47,7 @@ wheat5 <- readRDS("models/mod_wheat.rds")
 
 
 # Corn
-star1 <- stargazer(corn1, corn2, corn3, corn4, corn5,
+star1 <- stargazer(corn1, corn2, corn4, corn5,
                   align = FALSE, no.space = FALSE,
                   style = "aer", digits = 2,
                   omit = c("fips", "year", "state", "trend"),
@@ -62,23 +62,22 @@ star1 <- stargazer(corn1, corn2, corn3, corn4, corn5,
           apply.coef = multiply.100, apply.se = multiply.100,
           table.layout ="=dcm#-t-as=n",
           font.size = "footnotesize",
-          add.lines = list(c("Fixed-effect", "--", "County", "County", "County", "County"),
-                           c("Linear State-trend", "--", "--", "Yes", "Yes", "Yes"),
-                           c("Quad. State-trend", "--", "--", "--", "Yes", "Yes"),
-                           c("Clusterd SE", "--", "--", "--", "--", "State")),
+          add.lines = list(c("Fixed-effect", "--", "County", "County", "County"),
+                           c("Quad. State-trend", "--",  "--", "Yes", "Yes"),
+                           c("Clusterd SE", "--", "--", "--", "State")),
           notes.append = FALSE, notes.align = "l")
 
 
 loc <- which(star1 == "\\end{tabular} ")
 star1 <- star1[1:loc-1]
-star1notes <- paste("\\parbox{4.3in}{Notes: Table reports regression coefficients for log corn revenue per acre using degree day variables and precipitation variables from 1950-2010. Corn revenue per acre is calculated by multiplying production (lbs) per acre times average crop price from 1950-2010. Regression estimates are weighted by total county-level corn acres (smoothed using a spline). Estimates in \\textbf{bold} are statistically significant at 95\\%. Coefficients have been multiplied by 100.}")
+star1notes <- paste("\\parbox{3.7in}{Notes: Table reports regression coefficients for log corn revenue per acre using degree day variables and precipitation variables from 1950-2010. Corn revenue per acre is calculated by multiplying production (lbs) per acre times average crop price from 1950-2010. Regression estimates are weighted by total county-level corn acres (smoothed using a spline). Estimates in \\textbf{bold} are statistically significant at 95\\%. Coefficients have been multiplied by 100.}")
 
-star1 <- c(star1, "\\end{tabular}",star1notes)
+star1 <- c(star1, "\\end{tabular}", "\\begin{center}", star1notes, "\\end{center}")
 star1 <- c(star1, "\\end{table}")
 
 
 # cotton
-star2 <- stargazer(cotton1, cotton2, cotton3, cotton4, cotton5,
+star2 <- stargazer(cotton1, cotton2, cotton4, cotton5,
                   align = FALSE, no.space = FALSE,
                   style = "aer", digits = 2,
                   omit = c("fips", "year", "state", "trend"),
@@ -93,23 +92,22 @@ star2 <- stargazer(cotton1, cotton2, cotton3, cotton4, cotton5,
           apply.coef = multiply.100, apply.se = multiply.100,
           table.layout ="=dcm#-t-as=n",
           font.size = "footnotesize",
-          add.lines = list(c("Fixed-effect", "--", "County", "County", "County", "County"),
-                           c("Linear State-trend", "--", "--", "Yes", "Yes", "Yes"),
-                           c("Quad. State-trend", "--", "--", "--", "Yes", "Yes"),
-                           c("Clusterd SE", "--", "--", "--", "--", "State")),
+          add.lines = list(c("Fixed-effect", "--", "County", "County", "County"),
+                           c("Quad. State-trend", "--", "--", "Yes", "Yes"),
+                           c("Clusterd SE", "--", "--", "--", "State")),
           notes.append = FALSE, notes.align = "l")
 
 
 loc <- which(star2 == "\\end{tabular} ")
 star2 <- star2[1:loc-1]
-star2notes <- paste("\\parbox{4.3in}{Notes: Table reports regression coefficients for log cotton revenue per acre using degree day variables and precipitation variables from 1950-2010. Cotton revenue per acre is calculated by multiplying production (lbs) per acre times average crop price from 1950-2010. Regression estimates are weighted by total county-level cotton acres (smoothed using a spline). Estimates in \\textbf{bold} are statistically significant at 95\\%. Coefficients have been multiplied by 100.}")
+star2notes <- paste("\\parbox{3.7in}{Notes: Table reports regression coefficients for log cotton revenue per acre using degree day variables and precipitation variables from 1950-2010. Cotton revenue per acre is calculated by multiplying production (lbs) per acre times average crop price from 1950-2010. Regression estimates are weighted by total county-level cotton acres (smoothed using a spline). Estimates in \\textbf{bold} are statistically significant at 95\\%. Coefficients have been multiplied by 100.}")
 
-star2 <- c(star2, "\\end{tabular}",star2notes)
+star2 <- c(star2, "\\end{tabular}", "\\begin{center}", star2notes, "\\end{center}")
 star2 <- c(star2, "\\end{table}")
 
 
 # hay
-star3 <- stargazer(hay1, hay2, hay3, hay4, hay5,
+star3 <- stargazer(hay1, hay2,  hay4, hay5,
                   align = FALSE, no.space = FALSE,
                   style = "aer", digits = 2,
                   omit = c("fips", "year", "state", "trend"),
@@ -124,23 +122,22 @@ star3 <- stargazer(hay1, hay2, hay3, hay4, hay5,
           apply.coef = multiply.100, apply.se = multiply.100,
           table.layout ="=dcm#-t-as=n",
           font.size = "footnotesize",
-          add.lines = list(c("Fixed-effect", "--", "County", "County", "County", "County"),
-                           c("Linear State-trend", "--", "--", "Yes", "Yes", "Yes"),
-                           c("Quad. State-trend", "--", "--", "--", "Yes", "Yes"),
-                           c("Clusterd SE", "--", "--", "--", "--", "State")),
+          add.lines = list(c("Fixed-effect", "--", "County", "County",  "County"),
+                           c("Quad. State-trend", "--", "--", "Yes", "Yes"),
+                           c("Clusterd SE", "--", "--", "--", "State")),
           notes.append = FALSE, notes.align = "l")
 
 
 loc <- which(star3 == "\\end{tabular} ")
 star3 <- star3[1:loc-1]
-star3notes <- paste("\\parbox{4.3in}{Notes: Table reports regression coefficients for log hay revenue per acre using degree day variables and precipitation variables from 1950-2010. Hay revenue per acre is calculated by multiplying production (lbs) per acre times average crop price from 1950-2010. Regression estimates are weighted by total county-level hay acres (smoothed using a spline). Estimates in \\textbf{bold} are statistically significant at 95\\%. Coefficients have been multiplied by 100.}")
+star3notes <- paste("\\parbox{3.7in}{Notes: Table reports regression coefficients for log hay revenue per acre using degree day variables and precipitation variables from 1950-2010. Hay revenue per acre is calculated by multiplying production (lbs) per acre times average crop price from 1950-2010. Regression estimates are weighted by total county-level hay acres (smoothed using a spline). Estimates in \\textbf{bold} are statistically significant at 95\\%. Coefficients have been multiplied by 100.}")
 
-star3 <- c(star3, "\\end{tabular}",star3notes)
+star3 <- c(star3, "\\end{tabular}", "\\begin{center}", star3notes, "\\end{center}")
 star3 <- c(star3, "\\end{table}")
 
 
 # soybean
-star4 <- stargazer(soybean1, soybean2, soybean3, soybean4, soybean5,
+star4 <- stargazer(soybean1, soybean2, soybean4, soybean5,
                   align = FALSE, no.space = FALSE,
                   style = "aer", digits = 2,
                   omit = c("fips", "year", "state", "trend"),
@@ -155,23 +152,22 @@ star4 <- stargazer(soybean1, soybean2, soybean3, soybean4, soybean5,
           apply.coef = multiply.100, apply.se = multiply.100,
           table.layout ="=dcm#-t-as=n",
           font.size = "footnotesize",
-          add.lines = list(c("Fixed-effect", "--", "County", "County", "County", "County"),
-                           c("Linear State-trend", "--", "--", "Yes", "Yes", "Yes"),
-                           c("Quad. State-trend", "--", "--", "--", "Yes", "Yes"),
-                           c("Clusterd SE", "--", "--", "--", "--", "State")),
+          add.lines = list(c("Fixed-effect", "--", "County", "County", "County"),
+                           c("Quad. State-trend", "--", "--", "Yes", "Yes"),
+                           c("Clusterd SE", "--", "--", "--", "State")),
           notes.append = FALSE, notes.align = "l")
 
 
 loc <- which(star4 == "\\end{tabular} ")
 star4 <- star4[1:loc-1]
-star4notes <- paste("\\parbox{4.3in}{Notes: Table reports regression coefficients for log soybean revenue per acre using degree day variables and precipitation variables from 1950-2010. Soybean revenue per acre is calculated by multiplying production (lbs) per acre times average crop price from 1950-2010. Regression estimates are weighted by total county-level soybean acres (smoothed using a spline). Estimates in \\textbf{bold} are statistically significant at 95\\%. Coefficients have been multiplied by 100.}")
+star4notes <- paste("\\parbox{3.7in}{Notes: Table reports regression coefficients for log soybean revenue per acre using degree day variables and precipitation variables from 1950-2010. Soybean revenue per acre is calculated by multiplying production (lbs) per acre times average crop price from 1950-2010. Regression estimates are weighted by total county-level soybean acres (smoothed using a spline). Estimates in \\textbf{bold} are statistically significant at 95\\%. Coefficients have been multiplied by 100.}")
 
-star4 <- c(star4, "\\end{tabular}",star4notes)
+star4 <- c(star4, "\\end{tabular}", "\\begin{center}", star4notes, "\\end{center}")
 star4 <- c(star4, "\\end{table}")
 
 
 # wheat
-star5 <- stargazer(wheat1, wheat2, wheat3, wheat4, wheat5,
+star5 <- stargazer(wheat1, wheat2, wheat4, wheat5,
                   align = FALSE, no.space = FALSE,
                   style = "aer", digits = 2,
                   omit = c("fips", "year", "state", "trend"),
@@ -186,18 +182,17 @@ star5 <- stargazer(wheat1, wheat2, wheat3, wheat4, wheat5,
           apply.coef = multiply.100, apply.se = multiply.100,
           table.layout ="=dcm#-t-as=n",
           font.size = "footnotesize",
-          add.lines = list(c("Fixed-effect", "--", "County", "County", "County", "County"),
-                           c("Linear State-trend", "--", "--", "Yes", "Yes", "Yes"),
-                           c("Quad. State-trend", "--", "--", "--", "Yes", "Yes"),
-                           c("Clusterd SE", "--", "--", "--", "--", "State")),
+          add.lines = list(c("Fixed-effect", "--", "County",  "County", "County"),
+                           c("Quad. State-trend", "--", "--",  "Yes", "Yes"),
+                           c("Clusterd SE", "--", "--", "--", "State")),
           notes.append = FALSE, notes.align = "l")
 
 
 loc <- which(star5 == "\\end{tabular} ")
 star5 <- star5[1:loc-1]
-star5notes <- paste("\\parbox{4.3in}{Notes: Table reports regression coefficients for log wheat revenue per acre using degree day variables and precipitation variables from 1950-2010. Wheat revenue per acre is calculated by multiplying production (lbs) per acre times average crop price from 1950-2010. Regression estimates are weighted by total county-level wheat acres (smoothed using a spline). Estimates in \\textbf{bold} are statistically significant at 95\\%. Coefficients have been multiplied by 100.}")
+star5notes <- paste("\\parbox{3.7in}{Notes: Table reports regression coefficients for log wheat revenue per acre using degree day variables and precipitation variables from 1950-2010. Wheat revenue per acre is calculated by multiplying production (lbs) per acre times average crop price from 1950-2010. Regression estimates are weighted by total county-level wheat acres (smoothed using a spline). Estimates in \\textbf{bold} are statistically significant at 95\\%. Coefficients have been multiplied by 100.}")
 
-star5 <- c(star5, "\\end{tabular}",star5notes)
+star5 <- c(star5, "\\end{tabular}", "\\begin{center}", star5notes, "\\end{center}")
 star5 <- c(star5, "\\end{table}")
 
 
@@ -218,7 +213,7 @@ cat("\\end{document}", file = "tables.tex", append = TRUE)
 system("pdflatex tables.tex")
 
 }
-
+# 
 system("sed -r 's/([0-9\\.]+)\\$\\^\\{\\*{1,3}\\}\\$/\\\\textbf{\\1}/g' tables.tex > tables_out.tex")
 system("pdflatex tables_out.tex")
 
